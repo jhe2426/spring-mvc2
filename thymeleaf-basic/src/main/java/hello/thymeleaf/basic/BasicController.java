fee7f78ded17422b245ee63d2ac3047910fcc5d1
jhe2426 <jhe2426@naver.com>
2026-02-28 19:35:43 +0900

    자바스크립트 인라인

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -138,6 +138,15 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+
+        model.addAttribute("user", new User("User\"A\"", 10));
+        addUsers(model);
+
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("UserA", 10));
@@ -148,7 +157,7 @@ public class BasicController {
     }
 
     @Data
-    static class User {
+    public static class User {
         private String username;
         private int age;
 
