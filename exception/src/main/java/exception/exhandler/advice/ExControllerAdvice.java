9de2d392be0c736c9062a9a129f1eba6243c9c12
jhe2426 <jhe2426@naver.com>
2026-04-27 13:15:38 +0900

    @ControllerAdvice

---
+++ exception/src/main/java/exception/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,40 @@
+package exception.exhandler.advice;
+
+import exception.exception.UserException;
+import exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "exception.api")
+public class ExControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    // @ExceptionHandler(UserException.class) userExHandler 파라미터의 UserException이 발생될 때
+    // 이 ExceptionHandler를 호출하고 싶으면 (UserException.class)를 생략 할 수 있음
+    // @ExceptionHandler에 예외를 생략하면 메서드 파라미터의 예외로 지정이 된다.
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+}
